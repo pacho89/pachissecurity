62e73a0a3ac1a11474b3d14b64cc47e1f2045428
pacho89 <dannyfranciscohernandezgodoy@gmail.com>
2019-10-09 09:22:03 -0500

    listener mqtt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgxMqttLiteService} from 'ngx-mqtt-lite';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pachis';
+  vibracion = 'Terremoto';
+  gas = 'Quemando';
+  constructor(private ngxMqttLiteService: NgxMqttLiteService) {
+    
+  }
+
+  ngOnInit() {
+    this.ngxMqttLiteService.initializa('ws://192.168.25.251/ws', {
+      username: 'guest',
+      password: 'guest',
+      port: 15672,
+      keepalive: 15
+    });
+    this.ngxMqttLiteService.scope().subscribe(client => {
+      client.subscribe('home_out');
+    });
+    this.ngxMqttLiteService.listen('message').subscribe(data => {
+      console.log(data[1].toString());
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgxMqttLiteService} from 'ngx-mqtt-lite';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [NgxMqttLiteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
